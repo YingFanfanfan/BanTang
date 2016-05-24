07a587d4b4a7d16965d89fb8afeb0bdf7af92a5d
颖帆帆帆 <706533921@qq.com>
2016-05-24 11:36:54 +0800

    框架搭建, 自定义tabBar

---
+++ BanTang/BanTang/Classes/Main/Controller/BTTabBarVC.swift
@@ -0,0 +1,13 @@
+//
+//  BTTabBarVC.swift
+//  BanTang
+//
+//  Created by 颖帆帆帆 on 16/5/23.
+//  Copyright © 2016年 颖帆帆帆. All rights reserved.
+//
+
+import Cocoa
+
+class BTTabBarVC: UITabBarController {
+
+}
